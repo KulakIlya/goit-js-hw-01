56142251bb0034f4a335cd217a40471b95143f84
Ilya <kulak1224@gmail.com>
2023-12-19 13:39:46 +0200

    adding hw

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+function getElementWidth(content, padding, border) {
+  const contentVal = Number.parseFloat(content);
+  const paddingVal = Number.parseFloat(padding);
+  const borderVal = Number.parseFloat(border);
+
+  return contentVal + paddingVal + borderVal;
+}
